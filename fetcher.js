a21593b431044c16d126d55276ed86a85db73a0c
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-02-12 22:24:04 +0000

    sace file from url to path

---
+++ fetcher.js
@@ -0,0 +1,20 @@
+const request = require("request");
+const fs = require("fs");
+
+let URL = process.argv[2];
+//console.log(URL);
+let PATH = process.argv[3];
+//console.log(PATH);
+const fetcher = function (url, path) {
+  request(url, body => {
+      //console.log(body);
+     fs.writeFile(path, body, function(err) {
+      if (err) throw err;
+       console.log("Saved!");
+     });
+    }
+  );
+}
+
+fetcher(URL, PATH);
+
